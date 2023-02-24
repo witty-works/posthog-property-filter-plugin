8c387f1c2a93bc15e940313d22f556e42f8ea283
Tiina Turban <tiina303@gmail.com>
2023-02-24 15:56:16 +0100

    fix: don't add property values on missing top level filter part

--- index.test.js
+++ index.test.js
@@ -1,16 +1,27 @@
 const { createEvent } = require('@posthog/plugin-scaffold/test/utils')
 const { processEvent } = require('.')
 
-const global = { propertiesToFilter: ['gender', '$set.age', 'foo.bar.baz.one', 'nonExisting', '$set.$not_in_props'] }
+const global = {
+    propertiesToFilter: [
+        'gender',
+        '$set.gender',
+        '$set.age',
+        'foo.bar.baz.one',
+        'nonExisting',
+        '$set.$not_in_props',
+        'no-such.with-dot',
+]}
 
 const properties = {
     properties: {
         name: 'Mr. Hog',
         gender: 'male',
+        age: 12,
         $set: {
             age: 35,
             pet: 'dog',
             firstName: 'Post',
+            gender: 'female',
         },
         foo: {
             bar: {
@@ -33,6 +44,22 @@ test('event properties are filtered', async () => {
     expect(event.properties).toHaveProperty('foo')
     expect(event.properties.$set).toHaveProperty('firstName', 'Post')
     expect(event.properties.foo.bar.baz).toHaveProperty('two', 'two')
+    expect(event.properties).toEqual(
+    {
+        name: 'Mr. Hog',
+        age: 12,
+        $set: {
+            pet: 'dog',
+            firstName: 'Post',
+        },
+        foo: {
+            bar: {
+                baz: {
+                    two: 'two',
+                },
+            },
+        },
+    })
 })
 
 const emptyProperties = {}
